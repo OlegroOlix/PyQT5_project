3e5c302d205e0fbdc925283895c8a77d40aaad99
OlegroOlix <goliahovskiyoleg@gmail.com>
2023-09-30 13:05:50 +0300

    add new class Button

---
+++ button.py
@@ -0,0 +1,12 @@
+class Button:
+    def __init__(self, x: int, y: int, title: str):
+        self.x = x
+        self.y = y
+        self.title = title
+        self.appearance = True
+    def hide(self):
+        self.appearance = False     
+    def show(self):
+        self.appearance = True  
+    def print_statuse(self):
+        print(f"ані про віджет\n{self.title} {self.x}, {self.y} {self.appearance}")
